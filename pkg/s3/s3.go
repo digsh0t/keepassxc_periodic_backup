e5f70bb96086090907fffa3ba40059488237ebea
digsh0t <wintltr@gmail.com>
2023-08-03 22:44:43 +0700

    add s3 package for get and upload file

---
+++ pkg/s3/s3.go
@@ -0,0 +1,87 @@
+package s3
+
+import (
+	"context"
+	"errors"
+	"log"
+	"os"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/aws/aws-sdk-go-v2/service/s3/types"
+	"github.com/aws/smithy-go"
+)
+
+type BucketBasics struct {
+	S3Client *s3.Client
+}
+
+func GetBucket(bucketName string) (bool, error) {
+	// Load the Shared AWS Configuration (~/.aws/config)
+	cfg, err := config.LoadDefaultConfig(context.TODO())
+	if err != nil {
+		return false, err
+	}
+
+	// Create an Amazon S3 service client
+	client := s3.NewFromConfig(cfg)
+
+	// Get the first page of results for ListObjectsV2 for a bucket
+	_, err = client.HeadBucket(context.TODO(), &s3.HeadBucketInput{
+		Bucket: aws.String(bucketName),
+	})
+	exists := true
+	if err != nil {
+		var apiError smithy.APIError
+		if errors.As(err, &apiError) {
+			switch apiError.(type) {
+			case *types.NotFound:
+				log.Printf("Bucket %v is available.\n", bucketName)
+				exists = false
+				err = nil
+			default:
+				log.Printf("Either you don't have access to bucket %v or another error occurred. "+
+					"Here's what happened: %v\n", bucketName, err)
+			}
+		}
+	}
+
+	return exists, err
+}
+
+// UploadFile reads from a file and puts the data into an object in a bucket.
+func (basics BucketBasics) UploadFile(bucketName string, objectKey string, fileName string) error {
+	file, err := os.Open(fileName)
+	if err != nil {
+		log.Printf("Couldn't open file %v to upload. Here's why: %v\n", fileName, err)
+	} else {
+		defer file.Close()
+		_, err := basics.S3Client.PutObject(context.TODO(), &s3.PutObjectInput{
+			Bucket: aws.String(bucketName),
+			Key:    aws.String(objectKey),
+			Body:   file,
+		})
+		if err != nil {
+			log.Printf("Couldn't upload file %v to %v:%v. Here's why: %v\n",
+				fileName, bucketName, objectKey, err)
+		}
+	}
+	return err
+}
+
+func (bucket BucketBasics) GetFileEtag(bucketName string, objectKey string) (string, error) {
+	objectResult, err := bucket.S3Client.GetObjectAttributes(context.TODO(), &s3.GetObjectAttributesInput{
+		Bucket: aws.String(bucketName),
+		Key:    aws.String(objectKey),
+		ObjectAttributes: []types.ObjectAttributes{
+			types.ObjectAttributesChecksum,
+			types.ObjectAttributesObjectParts,
+			types.ObjectAttributesEtag,
+		},
+	})
+	if err != nil {
+		return "", err
+	}
+	return *(objectResult.ETag), nil
+}
